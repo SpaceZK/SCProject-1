56af2749ffde63aa444b2fd456b03e114123c725
SpaceZK <1209804676@qq.com>
2018-01-28 23:31:01 +0800

    modify

--- lib.js
+++ lib.js
@@ -1,11 +1,13 @@
+"use strict";
+
+let count = 0;
+
 $( document ).ready(function() {
  
     $( "#add_cutting_len" ).click(function( event ) {
- 
-        alert( "As you can see, the link no longer took you to jquery.com" );
- 
-        event.preventDefault();
- 
+		let  ele = $("<input/>");
+		ele.attr("type", "checkbox");
+		let tips = $("#cutting_len").val();
+    	$("#cutting_list_title").append("<br/>", ele, tips);
     });
- 
 });
