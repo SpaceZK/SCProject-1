055c6f959aa0d9bc3544a631d344b04ecdb734dd
SpaceZK <1209804676@qq.com>
2018-01-28 12:02:58 +0800

    quote jquery

--- lib.js
+++ lib.js
@@ -1,5 +1,10 @@
-"use strict";
-//---
-function add_cutting_length() {
-	alert("fuck!!");
-}
+require("jsdom").env("", function(err, window) {
+    if (err) {
+        console.error(err);
+        return;
+    }
+ 
+    var $ = require("jquery")(window);
+});
+
+alert("值为: " + $("#proto_len").val());
