ba6d3ff8ea3affe32bad0aac1930866c1101e6be
SpaceZK <1209804676@qq.com>
2018-02-19 01:08:18 +0800

    modify to express

--- main.js
+++ main.js
@@ -1,62 +1,27 @@
 'use strict';
 
-const electron = require('electron')
-// Module to control application life.
-const app = electron.app
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow
+const http = require('http');
+const fs = require('fs');
 
-const path = require('path')
-const url = require('url')
-
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
-
-function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
-
-  // and load the index.html of the app.
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
+function serveStaticFile(res, path, contentType, responseCode) {
+  if(!responseCode) responseCode = 200;
+  fs.readFile(__dirname + path, function(err, data) {
+    if(err) {
+      res.writeHead(500, {'Content-Type': 'text/plain'});
+      res.end('500 - Internal Error');
+    } else {
+      res.writeHead(responseCode, {'Content-Type': contentType});
+      res.end(data);
+    }
+  });
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
-
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
-
-app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
-})
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+http.createServer(function(req,res){
+  // 规范化url，去掉查询字符串、可选的反斜杠，并把它变成小写
+    var path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
+    switch(path) {
+      default:
+        serveStaticFile(res, '/public/index.html', 'text/html');
+        break;
+  }}).listen(3000);
+console.log('Server started on localhost:3000; press Ctrl-C to terminate....');
