ca031e8c1245d0de50f4f9ff645c21c98d94a4af
SpaceZK <1209804676@qq.com>
2018-04-05 16:12:41 +0800

    fixed

--- SCProject-QtVersion/DataTypes.cpp
+++ SCProject-QtVersion/DataTypes.cpp
@@ -10,18 +10,11 @@ bool CuttingData::AddCuttingItem(int length)
         }
     }
     CuttingItem ci(length, true);
-    m_lstCuttingItem.push_back(ci);
+    m_lstCuttingItems.push_back(ci);
     return true;
 }
 
 bool CuttingData::RemoveCuttingItem(int length)
 {
-    list<CuttingItem>::iterator it = m_lstCuttingItems.begin();
-    for( ; it != m_lstCuttingItems.end(); it++) {
-        if((*it).length == ci.length) {
-            return false;
-        }
-    }
-    m_lstCuttingItem.push_back(ci);
     return true;
 }
