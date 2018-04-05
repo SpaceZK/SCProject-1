e1e558de501832d757a6317d2e4d0b1da73bad09
SpaceZK <1209804676@qq.com>
2018-04-05 17:33:54 +0800

    add cutting functions

--- SCProject-QtVersion/CuttingFunctions.cpp
+++ SCProject-QtVersion/CuttingFunctions.cpp
@@ -1,6 +1,28 @@
 #include "CuttingFunctions.h"
+using namespace std;
 
-CuttingFunctions::CuttingFunctions()
-{
+CuttingFunctions::CuttingFunctions(int totalLength) {
+    m_iTotalLength = totalLength;
+}
+
+CuttingData CuttingFunctions::GenerateStatge() {
+    /************ DATA PREPROCESS *************/
     
+}
+
+int CuttingFunctions::GetTotalLength() {
+    return m_iTotalLength;
+}
+
+void CuttingFunctions::SetTotalLength(int len) {
+    m_iTotalLength = len;
+}
+
+list<ProtoType> CuttingFunctions::DynamicProcessCuttingData(std::list<ProtoType> data) {
+    //data check
+    if(m_iTotalLength <= 0) {
+        return ans
+    }
+    
+    list<ProtoType> ans;
 }
--- SCProject-QtVersion/CuttingFunctions.h
+++ SCProject-QtVersion/CuttingFunctions.h
@@ -3,14 +3,27 @@
 
 #include <DataTypes.h>
 
+struct ProtoType {
+    ProtoType(int c = 0, int v = 0) {
+        cost = c;
+        value = v;
+    }
+
+    int cost;
+    int value;
+};
+
 class CuttingFunctions
 {
 public:
-    CuttingFunctions();
-    
+    CuttingFunctions(int totalLength = 0);
+    CuttingData GenerateStatge();
+    int GetTotalLength(void);
+    void SetTotalLength(int len);
 private:
+    int m_iTotalLength;
     CuttingData m_CuttingData;
-    void __PreprocessCuttingData();
+    bool DynamicProcessProtoData(std::list<ProtoType> data);
 };
 
 #endif // CUTTINGFUNCTIONS_H
